fea30b5de2164c5a932fc5efaf4bf8544362dfcc
Mazen Abdelbasir <mazen.elba@gmail.com>
2020-09-12 11:43:27 -0400

    added spiral traverse

---
+++ Spiral Traverse/README.md
@@ -0,0 +1,16 @@
+Spiral Traverse
+
+Write a function that takes in an N x M two-dimensional array (squared or not squared) and returns a one-dimensional array of all the array's elements in spiral order.
+
+Spiral order starts at the top left corner of the two-dimensional array, goes to the right, and proceeds in a spiral pattern all the way until every element has been visited.
+
+INPUT
+array = [
+[1, 2, 3, 4],
+[12, 13, 14, 5],
+[11, 16, 15, 6],
+[10, 9, 8, 7],
+]
+
+OUTPUT
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
---
+++ Spiral Traverse/spiralTraverse.cpp
@@ -0,0 +1,53 @@
+#include <vector>
+using namespace std;
+
+vector<int> spiralTraverse(vector<vector<int>> array)
+{
+    if (array.size() == 0)
+    {
+        return {};
+    }
+
+    vector<int> result = {};
+    int startRow = 0;
+    int endRow = array.size() - 1;
+    int startCol = 0;
+    int endCol = array[0].size() - 1;
+
+    while (startRow <= endRow && startCol <= endCol)
+    {
+        for (int col = startCol; col <= endCol; col++)
+        {
+            result.push_back(array[startRow][col]);
+        }
+
+        for (int row = startRow + 1; row <= endRow; row++)
+        {
+            result.push_back(array[row][endCol]);
+        }
+
+        for (int col = endCol - 1; col >= startCol; col--)
+        {
+            if (startRow == endRow)
+            {
+                break;
+            }
+            result.push_back(array[endRow][col]);
+        }
+
+        for (int row = endRow - 1; row > startRow; row--)
+        {
+            if (startCol == endCol)
+            {
+                break;
+            }
+            result.push_back(array[row][startCol]);
+        }
+
+        startRow++;
+        endRow--;
+        startCol++;
+        endCol--;
+    }
+    return result;
+}
